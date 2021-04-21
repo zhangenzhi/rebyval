493906d74674bec108ac98f1fa7b9387870592fb
mi <zhangenzhi@xiaomi.com>
2021-04-21 17:26:05 +0800

    test: test conv net with bz 1024

--- examples/main.py
+++ examples/main.py
@@ -54,8 +54,7 @@ def get_dnn_target_net():
 def get_conv_target_net():
 
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu',input_shape=(32, 32, 3)),
         tf.keras.layers.MaxPooling2D((2, 2)),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
