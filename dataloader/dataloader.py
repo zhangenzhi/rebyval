82bb89ee28f49cf74e4176432e06a5eba0be1988
mi <zhangenzhi@xiaomi.com>
2021-04-21 15:34:24 +0800

    feat: add dnn for cifar10 and mnist

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,2 +1,56 @@
-import tensorflow as tf
 
+import os, fnmatch
+import tensorflow as tf
+from tensorflow.io import gfile
+
+
+class DataSpec:
+    def __init__(self, dataspec_args):
+        pass
+
+
+class DataLoader:
+    def __init__(self, dataloader_args):
+        self.dataloader_args = dataloader_args
+        self.dataspec = self._build_dataset_spec()
+
+    def _build_dataset_spec(self):
+        dataspec = DataSpec(self.dataloader_args['dataspec'])
+        return dataspec
+
+    def process_example(self, example):
+        pass
+
+    def load_dataset(self,
+                     input_dirs,
+                     glob_pattern,
+                     glob_recursively=False):
+
+        example_spec = self.dataspec
+
+        file_list = glob_tfrecords(input_dirs,
+                                   glob_pattern,
+                                   recursively=glob_recursively)
+        assert len(file_list) > 0, "No data files found."
+
+
+
+def glob_tfrecords(input_dirs, glob_pattern="example", recursively=False):
+    file_path_list = []
+    if isinstance(input_dirs, str):
+        input_dirs = [input_dirs]
+    for root_path in input_dirs:
+        assert gfile.exists(root_path), "{} does not exist.".format(root_path)
+        if not gfile.isdir(root_path):
+            file_path_list.append(root_path)
+            continue
+        if not recursively:
+            for filename in gfile.listdir(root_path):
+                if fnmatch.fnmatch(filename, glob_pattern):
+                    file_path_list.append(os.path.join(root_path, filename))
+        else:
+            for dir_path, _, filename_list in gfile.walk(root_path):
+                for filename in filename_list:
+                    if fnmatch.fnmatch(filename, glob_pattern):
+                        file_path_list.append(os.path.join(dir_path, filename))
+    return file_path_list
--- examples/main.py
+++ examples/main.py
@@ -4,11 +4,16 @@ import tensorflow as tf
 
 def get_target_dataset(name=None):
     if name == 'cifar10':
-        target_dataset = tf.keras.datasets.cifar10
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    elif name == 'mnist':
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     else:
         print("no such dataset")
         raise
-    return target_dataset
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(32)
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+    return train_dataset, test_dataset
 
 
 def get_value_net():
@@ -26,16 +31,17 @@ def get_value_net():
     return model
 
 
-def get_target_net():
+def get_dnn_target_net():
     model = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10000, activation='relu'),
+        tf.keras.layers.Dense(1000, activation='relu'),
+        tf.keras.layers.Dense(100, activation='relu'),
         tf.keras.layers.Dense(10, activation='softmax')
     ])
-
-    model.compile(optimizer='SGD',
-                  loss='sparse_categorical_crossentropy',
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+    model.compile(optimizer=optimizer,
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
     return model
 
@@ -44,28 +50,26 @@ def train_value_net():
     value_net = get_value_net()
 
 
-
 def train_target_net():
-    dataset = get_target_dataset('cifar10')
-    (x_train, y_train), (x_test, y_test) = dataset.load_data()
+    # train_dataset, test_dataset = get_target_dataset('cifar10')
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    # (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    train_net = get_dnn_target_net()
+    train_net.fit(x_train, y_train, batch_size= 32, epochs=100)
 
-    train_net = get_target_net()
-    train_net.fit(x_train, y_train, epochs=5)
+    train_net.evaluate(x_test, y_test, batch_size= 32, verbose=2)
 
-    train_net.evaluate(x_test, y_test, verbose=2)
 
 def save_train_net_vars(path, num, vars, val_loss):
     features = tf.train.Feature(feature={
-        "label":tf.train.Feature(float32_list=tf.train.Float32List(value=[val_loss])),
-        "target_weight":tf.train.Feature(float32_list=tf.train.Float32List(value=[vars]))
+        "label": tf.train.Feature(float32_list=tf.train.Float32List(value=[val_loss])),
+        "target_weight": tf.train.Feature(float32_list=tf.train.Float32List(value=[vars]))
     })
     example = tf.train.Example(features=features)
 
-    writer = tf.python_io.TFRecordWriter(os.path.join(path, num+".tfrecord"))
+    writer = tf.python_io.TFRecordWriter(os.path.join(path, num + ".tfrecord"))
     writer.write(example.SerializeToString())
     writer.close()
-
-
 
 
 def main():
