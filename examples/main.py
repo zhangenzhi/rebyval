60d053bd6a291348b96879463c18f7ab2ddee98c
mi <zhangenzhi@xiaomi.com>
2021-04-23 17:19:42 +0800

    test: test conv fitting for cifar100

--- examples/main.py
+++ examples/main.py
@@ -82,7 +82,7 @@ def train_target_net():
     # train_dataset, test_dataset = get_target_dataset('cifar10')
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data()
     # (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    train_net = get_dnn_target_net()
+    train_net = get_conv_target_net()
     train_net.fit(x_train, y_train, batch_size= 1024, epochs=1000000, validation_data=(x_test,y_test))
 
 
