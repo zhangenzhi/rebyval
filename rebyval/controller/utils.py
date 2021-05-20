046ea66c135fbd6022a6b51aafe6da5fbedd83c0
mi <zhangenzhi@xiaomi.com>
2021-05-20 16:20:40 +0800

    test: revval factor to fixed surrogate overfitting.

--- rebyval/controller/utils.py
+++ rebyval/controller/utils.py
@@ -1,6 +1,7 @@
 import os
 from rebyval.controller.constants import default_values_dict
 
+
 def check_args_from_yaml_content(yaml_content):
     checked_args = check_and_set_default_values(yaml_content)
     return checked_args
@@ -9,13 +10,27 @@ def check_args_from_yaml_content(yaml_content):
 def check_and_set_default_values(experiment_config):
     return experiment_config
 
-def valid_weights_pool(surrogate_args):
 
+def valid_weights_pool(surrogate_args):
     dataloader_args = surrogate_args['dataloader']
-    weights_pool_path = dataloader_args['datapath']
-    subfiles = os.listdir(weights_pool_path)
-    if subfiles == []:
-        print("No weights in the pool.")
-        raise("No weights in the pool.")
-    new_datapath = [os.path.join(weights_pool_path,f) for f in subfiles]
-    surrogate_args['dataloader']['datapath'] = new_datapath
+
+    if isinstance(surrogate_args['dataloader']['datapath'], str):
+        weights_pool_path = dataloader_args['datapath']
+        subfiles = os.listdir(weights_pool_path)
+        if subfiles == []:
+            print("No weights in the pool.")
+            raise ("No weights in the pool.")
+        new_datapath = [os.path.join(weights_pool_path, f) for f in subfiles]
+        surrogate_args['dataloader']['datapath'] = new_datapath
+
+    elif isinstance(surrogate_args['dataloader']['datapath'], list):
+        weights_pool_path = os.join(surrogate_args['dataloader']['datapath'], '..')
+        subfiles = os.listdir(weights_pool_path)
+        if subfiles == []:
+            print("No weights in the pool.")
+            raise ("No weights in the pool.")
+        new_datapath = [os.path.join(weights_pool_path, f) for f in subfiles]
+        surrogate_args['dataloader']['datapath'] = new_datapath
+
+    else:
+        raise ("No such type of datapath")
