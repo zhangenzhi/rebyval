b5b59f0761760874ed00937aed1c37dfac7b6efa
mi <zhangenzhi@xiaomi.com>
2021-05-24 17:39:57 +0800

    test: weights compression for surrogate model.

--- examples/imagenet.py
+++ examples/imagenet.py
@@ -14,15 +14,15 @@ if __name__ == '__main__':
     manual_dataset_dir = "/home/work/dataset/ILSVRC2012"
     # builder = tfds.builder(dataset_name, data_dir=manual_dataset_dir)
     # tfds.list_builders()
-    tfds.load('mnist')
-    # def load_ImageNet(ds_type, BASEDIR, batch_size):
-    #     [ds_train, ds_test], ds_info = tfds.load(ds_type, split=['train', 'validation'],
-    #                                              data_dir=BASEDIR, download=True, shuffle_files=True,
-    #                                              as_supervised=True, with_info=True,
-    #                                              download_and_prepare_kwargs={'download_dir': BASEDIR})
-    #
-    #     # ds_train = prepare_training(ds_train, batch_size)
-    #     # ds_test = prepare_test(ds_test, batch_size)
-    #     return [ds_train, ds_test], ds_info
-    #
-    # load_ImageNet(dataset_name,BASEDIR=manual_dataset_dir,batch_size=1024)
+    # tfds.load('mnist')
+    def load_ImageNet(ds_type, BASEDIR, batch_size):
+        [ds_train, ds_test], ds_info = tfds.load(ds_type, split=['train', 'validation'],
+                                                 data_dir=BASEDIR, download=True, shuffle_files=True,
+                                                 as_supervised=True, with_info=True,
+                                                 download_and_prepare_kwargs={'download_dir': BASEDIR})
+
+        # ds_train = prepare_training(ds_train, batch_size)
+        # ds_test = prepare_test(ds_test, batch_size)
+        return [ds_train, ds_test], ds_info
+
+    load_ImageNet(dataset_name,BASEDIR=manual_dataset_dir,batch_size=1024)
