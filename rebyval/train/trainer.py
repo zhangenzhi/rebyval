1cce149b27fdb8ef499ba268a36bbea822e14bd5
mi <zhangenzhi@xiaomi.com>
2021-05-24 15:41:30 +0800

    test: weights compression for surrogate model.

--- rebyval/train/trainer.py
+++ rebyval/train/trainer.py
@@ -217,7 +217,7 @@ class SurrogateTrainer(BaseTrainer):
             print_error("train step error")
             raise
 
-    # @tf.function(experimental_relax_shapes=True, experimental_compile=None)
+    @tf.function(experimental_relax_shapes=True, experimental_compile=None)
     def _train_step_surrogate_sum_reduce(self, inputs, labels):
         flat_vars = []
         # import pdb
