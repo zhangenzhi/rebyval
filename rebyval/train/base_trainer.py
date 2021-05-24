37fea4d3e8fa3449276ab0fac58063125afe2b95
mi <zhangenzhi@xiaomi.com>
2021-05-24 15:14:06 +0800

    test: weights compression for surrogate model.

--- rebyval/train/base_trainer.py
+++ rebyval/train/base_trainer.py
@@ -110,6 +110,7 @@ class BaseTrainer:
 
     def _build_enviroment(self):
         gpus = tf.config.experimental.list_physical_devices("GPU")
+        print(gpus)
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
 
