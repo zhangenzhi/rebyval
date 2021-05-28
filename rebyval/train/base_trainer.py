790127a00252a9533d013dfacd2e1fea10731e83
mi <zhangenzhi@xiaomi.com>
2021-05-28 16:08:03 +0800

    feat: add sum reduce weight saver

--- rebyval/train/base_trainer.py
+++ rebyval/train/base_trainer.py
@@ -588,6 +588,26 @@ class BaseTrainer:
                 feature[feature_name] = tf.train.Feature(float_list=tf.train.FloatList(value=value))
         return tf.train.Example(features=tf.train.Features(feature=feature))
 
+    def _during_train_vars_tensor_sum_reduce_example(self):
+        feature = {}
+        for feature_name, value in self.during_value_dict.items():
+            if isinstance(value, list):
+                model_vars = []
+                for tensor in value:
+                    axis = tensor.shape.rank - 1
+                    compressed_tensor = tf.math.reduce_sum(tensor, axis=axis, keepdims=True)
+                    model_vars.append(tf.reshape(compressed_tensor, shape=(-1)))
+                model_vars = tf.concat(model_vars, axis=0)
+                import pdb
+                pdb.set_trace()
+                value = tf.io.serialize_tensor(model_vars).numpy()
+                feature[feature_name] = tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+            else:
+                value = [value.numpy()]
+                feature[feature_name] = tf.train.Feature(float_list=tf.train.FloatList(value=value))
+        return tf.train.Example(features=tf.train.Features(feature=feature))
+
     def _during_train_vars_numpy_example(self):
         feature = {}
         for feature_name, value in self.during_value_dict.items():
@@ -608,6 +628,8 @@ class BaseTrainer:
     def _write_analyse_to_tfrecord(self):
 
         if self.valid_args['analyse']['format'] == 'tensor':
+            example = self._during_train_vars_tensor_example()
+        elif self.valid_args['analyse']['format'] == 'tensor_sum_reduce':
             example = self._during_train_vars_tensor_example()
         else:
             example = self._during_train_vars_numpy_example()
