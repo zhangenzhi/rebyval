0c45c4de7063008b597db0c4168874e6eb35b95c
mi <zhangenzhi@xiaomi.com>
2021-05-31 11:30:00 +0800

    debug: resnet50 reader for imagenet

--- examples/imagenet.py
+++ examples/imagenet.py
@@ -8,10 +8,10 @@ import tensorflow_datasets as tfds
 from tensorflow.keras import layers
 from rebyval.model.resnet import ResNet50
 
-def get_conv_target_net():
 
+def get_conv_target_net():
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu',input_shape=(256, 256, 3)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)),
         tf.keras.layers.MaxPooling2D((2, 2)),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
@@ -28,33 +28,40 @@ def get_conv_target_net():
 
     return model
 
+
 if __name__ == '__main__':
     # imagenet_ds = ImageNet2012()
     dataset_name = "imagenet2012"
     manual_dataset_dir = "/home/work/dataset/ILSVRC2012"
+
+
     # builder = tfds.builder(dataset_name, data_dir=manual_dataset_dir)
     # tfds.list_builders()
     # tfds.load('mnist')
     def load_ImageNet(ds_type, BASEDIR, batch_size):
-        [ds_train, ds_test], ds_info = tfds.load(ds_type, split=['train','validation'],
+        read_config = tfds.ReadConfig()
+        [ds_train, ds_test], ds_info = tfds.load(ds_type, split=['train', 'validation'],
                                                  data_dir=BASEDIR, download=True, shuffle_files=True,
-                                                 batch_size = batch_size, as_supervised=True, with_info=True)
+                                                 read_config=read_config,
+                                                 batch_size=batch_size, as_supervised=True, with_info=True)
 
-        resize_and_rescale = tf.keras.Sequential([layers.experimental.preprocessing.Resizing(256,256),
-                                                  layers.experimental.preprocessing.Rescaling(1./255.)])
+        resize_and_rescale = tf.keras.Sequential([layers.experimental.preprocessing.Resizing(256, 256),
+                                                  layers.experimental.preprocessing.Rescaling(1. / 255.)])
 
-        ds_train = ds_train.map(lambda x,y: (resize_and_rescale(x),y),num_parallel_calls=tf.data.AUTOTUNE)
+        ds_train = ds_train.interleave()
+        ds_train = ds_train.map(lambda x, y: (resize_and_rescale(x), y), num_parallel_calls=tf.data.AUTOTUNE)
+        ds_train = ds_train.cache()
         ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
 
-        ds_test = ds_test.map(lambda x,y: (resize_and_rescale(x),y),num_parallel_calls=tf.data.AUTOTUNE)
+        ds_test = ds_test.map(lambda x, y: (resize_and_rescale(x), y), num_parallel_calls=tf.data.AUTOTUNE)
         ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
         return [ds_train, ds_test], ds_info
 
-    [ds_train, ds_test], ds_info = load_ImageNet(dataset_name,BASEDIR=manual_dataset_dir,batch_size=256)
 
+    [ds_train, ds_test], ds_info = load_ImageNet(dataset_name, BASEDIR=manual_dataset_dir, batch_size=256)
 
     model = get_conv_target_net()
-    model.fit(ds_train, epochs=1,validation_data=ds_test)
+    model.fit(ds_train, epochs=1, validation_data=ds_test)
 
     # ResNet50
     # net = ResNet50()
