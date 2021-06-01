f96f3264bf074550b77cd1f6340e60b0f702d11f
mi <zhangenzhi@xiaomi.com>
2021-06-01 10:01:51 +0800

    test: larger weights pool for surrogate model.

--- examples/imagenet.py
+++ examples/imagenet.py
@@ -40,10 +40,9 @@ if __name__ == '__main__':
     # tfds.list_builders()
     # tfds.load('mnist')
     def load_ImageNet(ds_type, BASEDIR, batch_size):
-        read_config = tfds.ReadConfig()
+        # read_config = tfds.ReadConfig()
         [ds_train, ds_test], ds_info = tfds.load(ds_type, split=['train', 'validation'],
                                                  data_dir=BASEDIR, download=True, shuffle_files=True,
-                                                 read_config=read_config,
                                                  batch_size=batch_size, as_supervised=True, with_info=True)
         # ds_train = ds_train.interleave(lambda x,y: tf.data.TFRecordDataset(x),
         #                                block_length=256,
