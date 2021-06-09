726f8f5fb430f95aeb74708314463629394c7204
mi <zhangenzhi@xiaomi.com>
2021-06-09 11:02:40 +0800

    feat: imagenet distribute with tf mirror stragegy.

--- rebyval/model/dnn.py
+++ rebyval/model/dnn.py
@@ -7,7 +7,7 @@ class DenseNeuralNetwork(Model):
     def __init__(self, deep_dims, activations=None, regularizer=None):
         super(DenseNeuralNetwork, self).__init__()
         self.deep_dims = deep_dims
-        self.activations = activations if activations else ['relu']*len(deep_dims)
+        self.activations = activations if activations else ['relu'] * len(deep_dims)
         self.regularizer = regularizer
         self.dnn_layer = self.build_deep_layers()
 
@@ -18,8 +18,8 @@ class DenseNeuralNetwork(Model):
         for i in range(num_layers):
             dense_layer = Dense(
                 self.deep_dims[i],
-                bias_initializer = 'glorot_uniform',
-                kernel_regularizer = self.regularizer,
+                bias_initializer='glorot_uniform',
+                kernel_regularizer=self.regularizer,
                 activation=self.activations[i])
             deep_layers.append(dense_layer)
 
@@ -27,9 +27,11 @@ class DenseNeuralNetwork(Model):
 
     def call(self, x):
 
-        x_inputs = x['inputs']
+        if x.get('inputs'):
+            x_inputs = x['inputs']
+        else:
+            x_inputs = tf.reshape(x, shape=(x.shape[0], -1))
         next_input = Flatten()(x_inputs)
-        # next_input = next_input * next_input * 5
         for deep_layer in self.dnn_layer:
             next_input = deep_layer(next_input)
         return next_input
