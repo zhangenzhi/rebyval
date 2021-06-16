28c56fdfbb75377ce74d7da9c565be6d8551c4d7
mi <zhangenzhi@xiaomi.com>
2021-06-16 15:18:06 +0800

    test: debug multi-workers config.

--- examples/multi-dist.py
+++ examples/multi-dist.py
@@ -4,5 +4,6 @@ import json
 import pdb
 pdb.set_trace()
 f = open("./examples/experiment_configs/imagenet/tf_workers_config.json")
-os.environ['TF_CONFIG'] = json.load(f)
+tf_config = json.load(f)
+os.environ['TF_CONFIG'] = json.dumps(tf_config)
 strategy = tf.distribute.MultiWorkerMirroredStrategy()
