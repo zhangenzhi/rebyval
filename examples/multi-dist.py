8685f1e988515fdc809b00fbfa4da40083fb9872
mi <zhangenzhi@xiaomi.com>
2021-06-16 15:10:49 +0800

    test: debug multi-workers config.

--- examples/multi-dist.py
+++ examples/multi-dist.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 import json
 
-tf_config = json.loads("./examples/experiment_configs/tf_workers_config.json")
+tf_config = json.loads("./experiment_configs/tf_workers_config.json")
 strategy = tf.distribute.MultiWorkerMirroredStrategy()
