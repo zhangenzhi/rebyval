d791d1c7bcc82b965e645963ae359814fbcf4263
mi <zhangenzhi@xiaomi.com>
2021-06-21 10:03:51 +0800

    fix: fixed accuracy reset bugs

--- rebyval/optimizer/scheduler/linear_scaling_with_decay.py
+++ rebyval/optimizer/scheduler/linear_scaling_with_decay.py
@@ -21,7 +21,7 @@ class LinearScalingWithDecaySchedule(tf.keras.optimizers.schedules.LearningRateS
 
     def __call__(self, step):
         # lr decay
-        self.decay_lr = self.decay_lr * 0.1 if step + 1 % self.decay_steps == 0 else self.decay_lr
+        self.decay_lr = self.decay_lr * 0.1 if tf.math.mod(step + 1, self.decay_steps) == 0 else self.decay_lr
 
         # constant linear scaling
 
