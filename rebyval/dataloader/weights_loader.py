1e3bc361dfe54c97120cb700ed62883a9ad41cd2
zhangenzhi <zhangsuiyu657@gmail.com>
2022-03-26 10:58:46 +0800

    test: test on cloud.

--- rebyval/dataloader/weights_loader.py
+++ rebyval/dataloader/weights_loader.py
@@ -101,7 +101,7 @@ class DNNWeightsLoader(BaseDataLoader):
         full_dataset = self._load_analyse_tensor_from_tfrecord(filelist=filelist,
                                                                feature_config=feature_config)
         
-        train_dataset = full_dataset.take(self.info['train_samples']).shuffle(self.dataloader_args['batch_size'])
+        train_dataset = full_dataset.take(self.info['train_samples']).shuffle(self.dataloader_args['batch_size']*10)
         
         valid_dataset = full_dataset.skip(self.info['train_samples'])
         valid_dataset = valid_dataset.take(self.info['valid_samples']).shuffle(self.dataloader_args['batch_size'])
