20e2063111951500bb4e2674cfe18a5aba23b036
zhangenzhi <zhangsuiyu657@gmail.com>
2022-04-01 15:52:15 +0800

    feat: add replay buffer to weightloader.

--- scripts/cifar10_rebval.sh
+++ scripts/cifar10_rebval.sh
@@ -1,2 +1,2 @@
 LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4" \
-python main.py --config="./configs/cifar10/avglabel.yaml"
+python main.py --config="scripts/configs/cifar10/rebyval_dnn.yaml"
