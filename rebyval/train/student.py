a119f1afdaf442eadcdd7c807f0d0dc75ade98a9
zhangenzhi <zhangsuiyu657@gmail.com>
2022-04-04 20:34:23 +0800

    feat: multi processs training for student.

--- rebyval/train/student.py
+++ rebyval/train/student.py
@@ -24,8 +24,11 @@ class Student:
     def _build_enviroment(self):
         gpus = tf.config.experimental.list_physical_devices("GPU")
         print_green("devices:", gpus)
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+        except RuntimeError as e:
+            print(e)
         
 
     def _build_dataset(self):
