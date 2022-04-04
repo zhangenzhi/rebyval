560491c77af1faef97cd1dc533c1ea9f8dd9dfd7
zhangenzhi <zhangsuiyu657@gmail.com>
2022-04-04 19:12:50 +0800

    feat: multi processs training for student.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from time import sleep
 import multiprocessing as mp
 from tqdm import tqdm
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 import argparse
 
 from rebyval.controller.base_controller import BaseController
--- rebyval/controller/base_controller.py
+++ rebyval/controller/base_controller.py
@@ -90,7 +90,7 @@ class BaseController:
         for j in range(main_loop['nums']):
             processes = []
             for i in range(main_loop['student_nums']):
-                student = self._build_student(supervisor=None)
+                student = self._build_student(supervisor=self.supervisor)
                 p = Process(target = student.run, args=(self.queue,))
                 p.start()
                 processes.append(p)
