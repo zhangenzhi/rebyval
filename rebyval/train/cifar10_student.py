fd24f4fe00e91ac683342e0832e4e00d20b6ccaa
zhangenzhi <zhangsuiyu657@gmail.com>
2022-04-04 23:46:48 +0800

    feat: multi processs training for student.

--- rebyval/train/cifar10_student.py
+++ rebyval/train/cifar10_student.py
@@ -118,7 +118,7 @@ class Cifar10Student(Student):
         # import pdb
         # pdb.set_trace()
         if supervisor_vars != None:
-            # ForkedPdb().set_trace()
+            ForkedPdb().set_trace()
             self.supervisor_vars = supervisor_vars
             self.supervisor = self._build_supervisor_from_vars()
 
