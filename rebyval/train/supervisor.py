f4e8b4353d2512f766769b5184e0fab84b36377d
zhangenzhi <zhangsuiyu657@gmail.com>
2022-04-08 09:51:46 +0800

    test: neu loss suitable.

--- rebyval/train/supervisor.py
+++ rebyval/train/supervisor.py
@@ -30,8 +30,8 @@ class Supervisor:
     def _build_model(self):
         #TODO: need model registry
         model_args = self.args['model']
-        model = DNN(units=[64,32,16,1],
-                    activations=['relu', 'relu', 'relu', 'relu'],
+        model = DNN(units=[64,32,16,10],
+                    activations=['relu', 'relu', 'relu', 'sigmoid'],
                     use_neu=model_args['use_neu'],
                     use_bn=False,
                     seed=None)
