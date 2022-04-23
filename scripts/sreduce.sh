c8f25313889d909533752b5a9f01f01f3a336784
zhangenzhi <zhangsuiyu657@gmail.com>
2022-04-23 16:09:40 +0800

    fix: sum reduce config.

--- scripts/sreduce.sh
+++ scripts/sreduce.sh
@@ -1,2 +1,2 @@
 LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4" \
-python main.py --config="./scripts/configs/cifar10/multi-p.yaml" | tee log.txt 
+python main.py --config="./scripts/configs/cifar10/sreduce.yaml" | tee log.txt 
