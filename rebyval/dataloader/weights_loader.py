8ce9002e5e8b16e1416299b319ffa3dcf5f900c6
zhangenzhi <zhangsuiyu657@gmail.com>
2022-04-24 10:28:48 +0800

    fix: sum reduce config.

--- rebyval/dataloader/weights_loader.py
+++ rebyval/dataloader/weights_loader.py
@@ -126,9 +126,10 @@ class DNNWeightsLoader(BaseDataLoader):
         
         full_dataset = self._load_analyse_tensor_from_tfrecord(filelist=filelist,
                                                                feature_config=self.feature_config)
-        full_dataset = full_dataset.shuffle(self.info['total_samples'])
+        # full_dataset = full_dataset.shuffle(self.info['total_samples'])
         
         train_dataset = full_dataset.take(self.info['train_samples'])
+        train_dataset = train_dataset.shuffle(self.info['train_samples'])
         
         valid_dataset = full_dataset.skip(self.info['train_samples'])
         valid_dataset = valid_dataset.take(self.info['valid_samples'])
