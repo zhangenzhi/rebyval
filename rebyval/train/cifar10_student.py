7347468ca7f79f194e952fb4f727fdd2ebff5dca
zhangenzhi <zhangsuiyu657@gmail.com>
2022-05-01 04:42:19 +0000

    debug: reval test sgd.

--- rebyval/train/cifar10_student.py
+++ rebyval/train/cifar10_student.py
@@ -15,7 +15,7 @@ class Cifar10Student(Student):
         # self.supervisor_vars = supervisor_vars
         # self.supervisor = self._build_supervisor_from_vars()
     
-    @tf.function(experimental_relax_shapes=True, experimental_compile=None)
+    # @tf.function(experimental_relax_shapes=True, experimental_compile=None)
     def _train_step(self, inputs, labels, train_step = 0, epoch=0):
     
         try:
