8fed12142238315f0429d2036b43499009165c7e
zhangenzhi <zhangsuiyu657@gmail.com>
2022-05-02 08:03:02 +0000

    debug: on device.

--- rebyval/train/student.py
+++ rebyval/train/student.py
@@ -44,14 +44,10 @@ class Student:
             flat_vars.append(tf.reshape(sum_reduce, shape=(1, -1)))
         inputs = tf.concat(flat_vars, axis=1)
         
-        ForkedPdb().set_trace()
-        
         with tf.GradientTape() as tape:
             predictions = self.supervisor(inputs, training=True)
             loss = supervisor_loss_fn(labels, predictions)
-
             gradients = tape.gradient(loss, self.supervisor.trainable_variables)
-
             supervisor_opt.apply_gradients(
                 zip(gradients, self.supervisor.trainable_variables))
         print(loss)
