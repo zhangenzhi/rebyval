478a2ab025fcce902cc7ebb6d05609b332613c90
zhangenzhi <zhangsuiyu657@gmail.com>
2022-06-10 22:45:28 +0900

    fix: merge

--- rebyval/train/cifar10_student.py
+++ rebyval/train/cifar10_student.py
@@ -29,8 +29,8 @@ class Cifar10Student(Student):
                 predictions = self.model(inputs, training=True)
                 loss = self.loss_fn(labels, predictions)
             gradients = tape.gradient(loss, self.model.trainable_variables)
-            # norm_gard = gradients
-            norm_gard = [g/(1e-8+tf.norm(g)) for g in gradients]
+            norm_gard = gradients
+            # norm_gard = [g/(1e-8+tf.norm(g)) for g in gradients]
             self.optimizer.apply_gradients(
                 zip(norm_gard, self.model.trainable_variables))
         except:
