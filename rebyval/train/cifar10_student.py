fb55fda3625769de5154bba14b62f40b40af8e31
zezzz <zezzz@zezzz.imac.lab>
2022-06-10 15:50:00 +0900

    test: resnet sgd  01.

--- rebyval/train/cifar10_student.py
+++ rebyval/train/cifar10_student.py
@@ -31,7 +31,8 @@ class Cifar10Student(Student):
                 # ForkedPdb().set_trace()
                 loss = self.loss_fn(labels, predictions)
             gradients = tape.gradient(loss, self.model.trainable_variables)
-            norm_gard = [g/(1e-8+tf.norm(g)) for g in gradients]
+            norm_gard = gradients
+            # norm_gard = [g/(1e-8+tf.norm(g)) for g in gradients]
             self.optimizer.apply_gradients(
                 zip(norm_gard, self.model.trainable_variables))
         except:
