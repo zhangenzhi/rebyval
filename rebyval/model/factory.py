0a9148a944a6a5222bd861e062d57fbebdfc5122
zhangenzhi <zhangsuiyu657@gmail.com>
2022-06-11 10:08:03 +0900

    test: keras resnet50.

--- rebyval/model/factory.py
+++ rebyval/model/factory.py
@@ -2,12 +2,16 @@ from .dnn import DNN
 from .cnn import CNN
 from .resnet import ResNet50
 from .vgg import VGG16, VGG11
-
+from tensorflow.keras.applications.resnet50 import ResNet50 as kResNet50
 
 
 class ModelFactory():
     def __init__(self) -> None:
-        self.model_list = {'dnn':DNN, 'cnn':CNN, 'resnet50':ResNet50, 'vgg16':VGG16,'vgg11':VGG11}
+        self.model_list = {'dnn':DNN, 'cnn':CNN, 
+                           'resnet50':ResNet50,
+                           'keras-resnet50':kResNet50, 
+                           'vgg16':VGG16,
+                           'vgg11':VGG11}
 
     def __call__(self, model_args):
         return self.get_model(model_args=model_args)
