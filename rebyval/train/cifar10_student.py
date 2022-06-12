8c3fa0518925fd8641c10799cff53bb521fbe67d
zhangenzhi <zhangsuiyu657@gmail.com>
2022-06-13 08:43:24 +0900

    test: handcradt resnet56 via andm.

--- rebyval/train/cifar10_student.py
+++ rebyval/train/cifar10_student.py
@@ -205,10 +205,10 @@ class Cifar10Student(Student):
 
                 # lr decay
                 if epoch/self.dataloader.info['epochs'] == 0.5:
-                    self.optimizer.learning_rate = self.optimizer.learning_rate*0.5
+                    self.optimizer.learning_rate = self.optimizer.learning_rate*0.1
                     print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
                 elif epoch/self.dataloader.info['epochs'] == 0.75:
-                    self.optimizer.learning_rate = self.optimizer.learning_rate*0.5
+                    self.optimizer.learning_rate = self.optimizer.learning_rate*0.1
                     print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
 
                 with trange(self.dataloader.info['train_step'], desc="Train steps", leave=False) as t:
