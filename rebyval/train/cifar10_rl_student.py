5765348da07a5a2179a53610803162685909d05b
zhangenzhi <zhangsuiyu657@gmail.com>
2022-06-16 22:52:30 +0900

    feat: rl students.

--- rebyval/train/cifar10_rl_student.py
+++ rebyval/train/cifar10_rl_student.py
@@ -130,8 +130,8 @@ class Cifar10RLStudent(Student):
                                     v.set_postfix(sv_loss=v_loss.numpy())
                                     vv_metrics.append(v_metrics)
                                 ev_loss = self.mv_loss_fn.result()
-                                ett_metric = tf.reduce_mean(tt_metrics)
-                            # self._write_trail_to_tfrecord(states=self.model.trainable_weights, rewards=ett_metric, actions=action, step=self.gloabl_train_step)
+                                vv_metrics = tf.reduce_mean(vv_metrics)
+                            # self._write_trail_to_tfrecord(states=self.model.trainable_weights, rewards=vv_metrics, actions=action, step=self.gloabl_train_step)
                     et_loss = self.mt_loss_fn.result()
                 
                 with trange(self.dataloader.info['test_step'], desc="Test steps") as t:
