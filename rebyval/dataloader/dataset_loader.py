90d83b9b4e27d8433e01b01874bca257f12f1b1f
zezzz <zezzz@zezzz.imac.lab>
2022-06-21 02:12:04 +0000

    feat: add log loss with tloss less 1.0

--- rebyval/dataloader/dataset_loader.py
+++ rebyval/dataloader/dataset_loader.py
@@ -109,7 +109,7 @@ class Cifar10DataLoader(BaseDataLoader):
         train_dataset = full_dataset.take(train_size)
         train_dataset = train_dataset.batch(self.dataloader_args['batch_size'])
         # data augmentation
-        train_dataset = train_dataset.map(lambda x:{'inputs':data_augmentation(x['inputs']),'labels': x['labels']}, num_parallel_calls=16)
+        # train_dataset = train_dataset.map(lambda x:{'inputs':data_augmentation(x['inputs']),'labels': x['labels']}, num_parallel_calls=16)
         train_dataset = train_dataset.prefetch(1)
         train_dataset = train_dataset.repeat(epochs)
 
