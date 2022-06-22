8d9e0ddd4ed156b8c62f6291210336eaae83670e
zezzz <zezzz@zezzz.imac.lab>
2022-06-22 01:51:09 +0000

    refactor: add necessary to base student.

--- rebyval/train/cifar10_student.py
+++ rebyval/train/cifar10_student.py
@@ -1,5 +1,4 @@
-from matplotlib.pyplot import sca
-from sklearn import metrics
+
 from tqdm import trange
 import numpy as np
 import tensorflow as tf
@@ -17,28 +16,6 @@ class Cifar10Student(Student):
 
         self.action_space = np.random.uniform(low=1.0, high=1.0, size=100)
         self.index_min = 0
-
-        # self.supervisor_vars = supervisor_vars
-        # self.supervisor = self._build_supervisor_from_vars()
-    
-    @tf.function(experimental_relax_shapes=True, experimental_compile=None)
-    def _train_step(self, inputs, labels):
-    
-        try:
-            with tf.GradientTape() as tape:
-                predictions = self.model(inputs, training=True)
-                loss = self.loss_fn(labels, predictions)
-            gradients = tape.gradient(loss, self.model.trainable_variables)
-            norm_gard = gradients
-            # norm_gard = [g/(1e-8+tf.norm(g)) for g in gradients]
-            self.optimizer.apply_gradients(
-                zip(norm_gard, self.model.trainable_variables))
-        except:
-            print_error("train step error")
-            raise
-        self.mt_loss_fn.update_state(loss)
-        
-        return loss
     
     def weightspace_loss(self, weights):
         # label
@@ -51,182 +28,103 @@ class Cifar10Student(Student):
         s_loss = tf.squeeze(s_loss)
         return s_loss
 
-    def _rl_train_step(self, inputs, labels, train_step = 0, epoch=0, action_space=[0.01, 1.5]):
+
+    # @tf.function(experimental_relax_shapes=True, experimental_compile=None)
+    # def _valid_step(self, inputs, labels):
+    #     predictions = self.model(inputs, training=False)
+    #     loss = self.loss_fn(labels, predictions)
+    #     self.mv_loss_fn.update_state(loss)
+    #     return loss
+
+    # @tf.function(experimental_relax_shapes=True, experimental_compile=None)
+    # def _test_step(self, inputs, labels):
+    #     predictions = self.model(inputs, training=False)
+    #     loss = self.loss_fn(labels, predictions)
+    #     test_metrics = tf.reduce_mean(self.test_metrics(labels, predictions))
+    #     self.mtt_loss_fn.update_state(loss)
+    #     return loss, test_metrics
+
+    # def train(self, new_student=None, supervisor_info=None):
         
-        step = train_step+epoch*self.dataloader.info['train_step']
+    #     # parse train loop control args
+    #     train_loop_args = self.args['train_loop']
+    #     train_args = train_loop_args['train']
+    #     valid_args = train_loop_args['valid']
+    #     test_args = train_loop_args['test']
 
-        # base direction 
-        with tf.GradientTape() as tape_t:
-            predictions = self.model(inputs, training=True)
-            t_loss = self.loss_fn(labels, predictions)
-        t_grad = tape_t.gradient(t_loss, self.model.trainable_variables)
-        norm_gard = [g/(1e-8+tf.norm(g)) for g in t_grad]
+    #     # dataset train, valid, test
+    #     train_iter = iter(self.train_dataset)
+    #     valid_iter = iter(self.valid_dataset)
+    #     test_iter = iter(self.test_dataset)
         
-        flat_grads = [tf.reshape(tf.math.reduce_sum(g, axis= -1), shape=(-1)) for g in norm_gard]
-        flat_vars = [tf.reshape(tf.math.reduce_sum(v, axis= -1), shape=(-1)) for v in self.model.trainable_variables] 
-        flat_grad = tf.reshape(tf.concat(flat_grads, axis=0), (1,-1))
-        flat_var = tf.reshape(tf.concat(flat_vars, axis=0), (1,-1))
-
-        # sample action
-        action_sample = tf.random.uniform(minval=action_space[0], maxval=action_space[1], shape=(100,1))
-        scaled_gards = flat_grad * action_sample
-        var_copy = tf.reshape(tf.tile(flat_var, [scaled_gards.shape.as_list()[0], 1]), scaled_gards.shape)
-        scaled_vars = var_copy - scaled_gards * 0.1
-        values = self.supervisor(scaled_vars)
-        index_min = min(range(len(values)), key=values.__getitem__)
-
-        # next state
-        gradients = [g*action_sample[index_min] for g in norm_gard]
-        self.optimizer.apply_gradients(
-            zip(gradients, self.model.trainable_variables))
         
-        with self.logger.as_default():
-            tf.summary.scalar("surrogate_loss", values[index_min], step=step)
-            tf.summary.scalar("action", action_space[index_min], step=step)
-            tf.summary.histogram("values", values, step=step)
-            
-        self.mt_loss_fn.update_state(t_loss)
-        return t_loss
+    #     if supervisor_info != None:
+    #         # self.supervisor_info = supervisor_info
+    #         self.supervisor = self._build_supervisor_from_vars(supervisor_info)
 
-    def _reval_train_step(self, inputs, labels, train_step = 0, epoch=0, actions=[0.0, 2.0]):
-        
-        step = train_step+epoch*self.dataloader.info['train_step']
+    #     # train, valid, write to tfrecords, test
+    #     # tqdm update, logger
+    #     with trange(self.dataloader.info['epochs'], desc="Epochs") as e:
+    #         for epoch in e:
+    #             # lr decay
+    #             if int(self.dataloader.info['epochs']*0.5) <= epoch < int(self.dataloader.info['epochs']*0.75):
+    #                 self.optimizer.learning_rate = 0.0001
+    #                 print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
+    #             elif int(self.dataloader.info['epochs']*0.75) <= epoch < int(self.dataloader.info['epochs']*0.95):
+    #                 self.optimizer.learning_rate = 0.00001
+    #                 print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
+    #             elif int(self.dataloader.info['epochs']*0.95) <= epoch:
+    #                 self.optimizer.learning_rate = 0.000001
+    #                 print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
 
-        # base direction 
-        with tf.GradientTape() as tape_t:
-            predictions = self.model(inputs, training=True)
-            t_loss = self.loss_fn(labels, predictions)
-        t_grad = tape_t.gradient(t_loss, self.model.trainable_variables)
-        norm_gard = [g/(1e-8+tf.norm(g)) for g in t_grad]
-        
-        # sample actions per 10 steps, add baseline per 10 samples 
-        if self.id % 10 != 0:
-            if (step+1) % 10 == 0:
-                values = []
-                self.action_space = np.random.uniform(low=actions[0], high=actions[1], size=100)
-                for action in self.action_space:
-                    scaled_grad = [g*action for g in norm_gard]
-                    n_s = [s - 0.1*g*10 for s,g in zip(self.model.trainable_variables, scaled_grad)]
-                    v = self.weightspace_loss(n_s)
-                    values.append(v)
-                self.index_min = min(range(len(values)), key=values.__getitem__)
-                with self.logger.as_default():
-                    tf.summary.scalar("surrogate_loss", values[self.index_min], step=step)
-                    tf.summary.histogram("values", tf.concat(values, axis=0), step=step)
-    
-        # next_state
-        gradients = [g*self.action_space[self.index_min] for g in norm_gard]
-        self.optimizer.apply_gradients(
-            zip(gradients, self.model.trainable_variables))
-        
-        with self.logger.as_default():
-            tf.summary.scalar("action", self.action_space[self.index_min], step=step)
-
-        self.mt_loss_fn.update_state(t_loss)
-        return t_loss
-
-    def _valid_step(self, inputs, labels, valid_step = 0, epoch=0, weight_space=None):
-        
-        # step = valid_step + epoch*self.dataloader.info['valid_step']
-
-        predictions = self.model(inputs, training=False)
-        loss = self.loss_fn(labels, predictions)
-        self.mv_loss_fn.update_state(loss)
-        # with self.logger.as_default():
-        #     tf.summary.scalar("v_loss", loss, step=step)
-        return loss
-
-    @tf.function(experimental_relax_shapes=True, experimental_compile=None)
-    def _test_step(self, inputs, labels):
-        predictions = self.model(inputs, training=False)
-        loss = self.loss_fn(labels, predictions)
-        test_metrics = tf.reduce_mean(self.test_metrics(labels, predictions))
-        self.mtt_loss_fn.update_state(loss)
-        return loss, test_metrics
-
-    def train(self, new_student=None, supervisor_info=None):
-        
-        # parse train loop control args
-        train_loop_args = self.args['train_loop']
-        train_args = train_loop_args['train']
-        valid_args = train_loop_args['valid']
-        test_args = train_loop_args['test']
-
-        # dataset train, valid, test
-        train_iter = iter(self.train_dataset)
-        valid_iter = iter(self.valid_dataset)
-        test_iter = iter(self.test_dataset)
-        
-        # metrics reset
-        # self.test_metrics.reset_states()
-        
-        if supervisor_info != None:
-            # self.supervisor_info = supervisor_info
-            self.supervisor = self._build_supervisor_from_vars(supervisor_info)
-
-        # train, valid, write to tfrecords, test
-        # tqdm update, logger
-        with trange(self.dataloader.info['epochs'], desc="Epochs") as e:
-            for epoch in e:
-                # lr decay
-                if int(self.dataloader.info['epochs']*0.5) <= epoch < int(self.dataloader.info['epochs']*0.75):
-                    self.optimizer.learning_rate = 0.0001
-                    print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
-                elif int(self.dataloader.info['epochs']*0.75) <= epoch < int(self.dataloader.info['epochs']*0.95):
-                    self.optimizer.learning_rate = 0.00001
-                    print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
-                elif int(self.dataloader.info['epochs']*0.95) <= epoch:
-                    self.optimizer.learning_rate = 0.000001
-                    print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
-
-                with trange(self.dataloader.info['train_step'], desc="Train steps", leave=False) as t:
-                    self.mt_loss_fn.reset_states()
-                    for train_step in t:
-                        data = train_iter.get_next()
-                        if self.supervisor == None:
-                            train_loss = self._train_step(data['inputs'], data['labels'])
-                        else:
-                            train_loss = self._rebyval_train_step(data['inputs'], data['labels'], 
-                                                        train_step=train_step, epoch=epoch)
-                        t.set_postfix(st_loss=train_loss.numpy())
+    #             with trange(self.dataloader.info['train_step'], desc="Train steps", leave=False) as t:
+    #                 self.mt_loss_fn.reset_states()
+    #                 for train_step in t:
+    #                     data = train_iter.get_next()
+    #                     if self.supervisor == None:
+    #                         train_loss = self._train_step(data['inputs'], data['labels'])
+    #                     else:
+    #                         train_loss = self._rebyval_train_step(data['inputs'], data['labels'], 
+    #                                                     train_step=train_step, epoch=epoch)
+    #                     t.set_postfix(st_loss=train_loss.numpy())
                         
-                        if train_step % valid_args['valid_gap'] == 0:
-                            with trange(self.dataloader.info['valid_step'], desc="Valid steps", leave=False) as v:
-                                self.mv_loss_fn.reset_states()
-                                for valid_step in v:
-                                    v_data = valid_iter.get_next()
-                                    valid_loss = self._valid_step(v_data['inputs'], v_data['labels'],
-                                                                valid_step=valid_step, epoch=epoch, 
-                                                                weight_space=valid_args['weight_space'])
-                                    v.set_postfix(sv_loss=valid_loss.numpy())
-                                ev_loss = self.mv_loss_fn.result()
-                                # online update supervisor
-                                if self.supervisor != None:
-                                    self.update_supervisor(self.model.trainable_variables, ev_loss)
-                                # self._write_trace_to_tfrecord(weights = self.model.trainable_variables, 
-                                #                               valid_loss = ev_loss,
-                                #                               weight_space = valid_args['weight_space'])
-                    et_loss = self.mt_loss_fn.result()
+    #                     if train_step % valid_args['valid_gap'] == 0:
+    #                         with trange(self.dataloader.info['valid_step'], desc="Valid steps", leave=False) as v:
+    #                             self.mv_loss_fn.reset_states()
+    #                             for valid_step in v:
+    #                                 v_data = valid_iter.get_next()
+    #                                 valid_loss = self._valid_step(v_data['inputs'], v_data['labels'],
+    #                                                             valid_step=valid_step, epoch=epoch, 
+    #                                                             weight_space=valid_args['weight_space'])
+    #                                 v.set_postfix(sv_loss=valid_loss.numpy())
+    #                             ev_loss = self.mv_loss_fn.result()
+    #                             # online update supervisor
+    #                             if self.supervisor != None:
+    #                                 self.update_supervisor(self.model.trainable_variables, ev_loss)
+    #                             # self._write_trace_to_tfrecord(weights = self.model.trainable_variables, 
+    #                             #                               valid_loss = ev_loss,
+    #                             #                               weight_space = valid_args['weight_space'])
+    #                 et_loss = self.mt_loss_fn.result()
                 
-                with trange(self.dataloader.info['test_step'], desc="Test steps") as t:
-                    self.mtt_loss_fn.reset_states()
-                    tt_metrics = []
-                    for test_step in t:
-                        # self.test_metrics.reset_states()
-                        t_data = test_iter.get_next()
-                        t_loss,t_metric = self._test_step(t_data['inputs'], t_data['labels'])
-                        t.set_postfix(test_loss=t_loss.numpy())
-                        tt_metrics.append(t_metric)
-                    ett_loss = self.mtt_loss_fn.result()
-                    ett_metric = tf.reduce_mean(tt_metrics)
+    #             with trange(self.dataloader.info['test_step'], desc="Test steps") as t:
+    #                 self.mtt_loss_fn.reset_states()
+    #                 tt_metrics = []
+    #                 for test_step in t:
+    #                     # self.test_metrics.reset_states()
+    #                     t_data = test_iter.get_next()
+    #                     t_loss,t_metric = self._test_step(t_data['inputs'], t_data['labels'])
+    #                     t.set_postfix(test_loss=t_loss.numpy())
+    #                     tt_metrics.append(t_metric)
+    #                 ett_loss = self.mtt_loss_fn.result()
+    #                 ett_metric = tf.reduce_mean(tt_metrics)
                     
-                e.set_postfix(et_loss=et_loss.numpy(), ett_metric=ett_metric.numpy(), ett_loss=ett_loss.numpy())
-                with self.logger.as_default():
-                    tf.summary.scalar("et_loss", et_loss, step=epoch)
-                    tf.summary.scalar("ev_loss", ev_loss, step=epoch)
-                    tf.summary.scalar("ett_mloss", ett_loss, step=epoch)
-                    tf.summary.scalar("ett_metric", ett_metric, step=epoch)
-        self.model.summary()
+    #             e.set_postfix(et_loss=et_loss.numpy(), ett_metric=ett_metric.numpy(), ett_loss=ett_loss.numpy())
+    #             with self.logger.as_default():
+    #                 tf.summary.scalar("et_loss", et_loss, step=epoch)
+    #                 tf.summary.scalar("ev_loss", ev_loss, step=epoch)
+    #                 tf.summary.scalar("ett_mloss", ett_loss, step=epoch)
+    #                 tf.summary.scalar("ett_metric", ett_metric, step=epoch)
+    #     self.model.summary()
                 
                 
         
--- rebyval/train/student.py
+++ rebyval/train/student.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import trange
 from datetime import datetime
 import tensorflow as tf
 
@@ -10,7 +11,6 @@ from rebyval.dataloader.dataset_loader import Cifar10DataLoader
 from rebyval.model.factory import model_factory
 
 # others
-from rebyval.optimizer.scheduler.linear_scaling_with_warmup import LinearScalingWithWarmupSchedule
 from rebyval.train.utils import ForkedPdb
 from rebyval.tools.utils import print_green, print_error, print_normal, check_mkdir, save_yaml_contents
 from rebyval.dataloader.utils import glob_tfrecords
@@ -133,7 +133,22 @@ class Student:
 
     @tf.function(experimental_relax_shapes=True, experimental_compile=None)
     def _train_step(self, inputs, labels):
-        raise NotImplementedError("student need _train_step func.")
+    
+        try:
+            with tf.GradientTape() as tape:
+                predictions = self.model(inputs, training=True)
+                loss = self.loss_fn(labels, predictions)
+            gradients = tape.gradient(loss, self.model.trainable_variables)
+            norm_gard = gradients
+            # norm_gard = [g/(1e-8+tf.norm(g)) for g in gradients]
+            self.optimizer.apply_gradients(
+                zip(norm_gard, self.model.trainable_variables))
+        except:
+            print_error("train step error")
+            raise
+        self.mt_loss_fn.update_state(loss)
+        
+        return loss
 
     @tf.function(experimental_relax_shapes=True, experimental_compile=None)
     def _rebyval_train_step(self, inputs, labels):
@@ -141,28 +156,99 @@ class Student:
 
     @tf.function(experimental_relax_shapes=True, experimental_compile=None)
     def _valid_step(self, inputs, labels):
-        raise NotImplementedError("student need _valid_step func.")
+        predictions = self.model(inputs, training=False)
+        loss = self.loss_fn(labels, predictions)
+        self.mv_loss_fn.update_state(loss)
+        return loss
 
     @tf.function(experimental_relax_shapes=True, experimental_compile=None)
     def _test_step(self, inputs, labels):
-        raise NotImplementedError("student need _test_step func.")
+        predictions = self.model(inputs, training=False)
+        loss = self.loss_fn(labels, predictions)
+        test_metrics = tf.reduce_mean(self.test_metrics(labels, predictions))
+        self.mtt_loss_fn.update_state(loss)
+        return loss, test_metrics
 
-    def train(self):
-
+    def train(self, new_student=None, supervisor_info=None):
+        
         # parse train loop control args
         train_loop_args = self.args['train_loop']
         train_args = train_loop_args['train']
+        valid_args = train_loop_args['valid']
+        test_args = train_loop_args['test']
 
         # dataset train, valid, test
         train_iter = iter(self.train_dataset)
         valid_iter = iter(self.valid_dataset)
         test_iter = iter(self.test_dataset)
+        
+        
+        if supervisor_info != None:
+            # self.supervisor_info = supervisor_info
+            self.supervisor = self._build_supervisor_from_vars(supervisor_info)
 
-        # metrics reset
-        metric_name = self.args['metrics']['name']
-        self.metrics[metric_name].reset_states()
+        # train, valid, write to tfrecords, test
+        # tqdm update, logger
+        with trange(self.dataloader.info['epochs'], desc="Epochs") as e:
+            for epoch in e:
+                # lr decay
+                if int(self.dataloader.info['epochs']*0.5) <= epoch < int(self.dataloader.info['epochs']*0.75):
+                    self.optimizer.learning_rate = 0.0001
+                    print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
+                elif int(self.dataloader.info['epochs']*0.75) <= epoch < int(self.dataloader.info['epochs']*0.95):
+                    self.optimizer.learning_rate = 0.00001
+                    print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
+                elif int(self.dataloader.info['epochs']*0.95) <= epoch:
+                    self.optimizer.learning_rate = 0.000001
+                    print("Current decayed learning rate is {}".format(self.optimizer.learning_rate))
 
-        raise NotImplementedError("need train, valid, test logic.")
+                with trange(self.dataloader.info['train_step'], desc="Train steps", leave=False) as t:
+                    self.mt_loss_fn.reset_states()
+                    for train_step in t:
+                        data = train_iter.get_next()
+                        if self.supervisor == None:
+                            train_loss = self._train_step(data['inputs'], data['labels'])
+                        else:
+                            train_loss = self._rebyval_train_step(data['inputs'], data['labels'], 
+                                                        train_step=train_step, epoch=epoch)
+                        t.set_postfix(st_loss=train_loss.numpy())
+                        
+                        if train_step % valid_args['valid_gap'] == 0:
+                            with trange(self.dataloader.info['valid_step'], desc="Valid steps", leave=False) as v:
+                                self.mv_loss_fn.reset_states()
+                                for valid_step in v:
+                                    v_data = valid_iter.get_next()
+                                    valid_loss = self._valid_step(v_data['inputs'], v_data['labels'],
+                                                                valid_step=valid_step, epoch=epoch, 
+                                                                weight_space=valid_args['weight_space'])
+                                    v.set_postfix(sv_loss=valid_loss.numpy())
+                                ev_loss = self.mv_loss_fn.result()
+                                # online update supervisor
+                                if self.supervisor != None:
+                                    self.update_supervisor(self.model.trainable_variables, ev_loss)
+                                # self._write_trace_to_tfrecord(weights = self.model.trainable_variables, 
+                                #                               valid_loss = ev_loss,
+                                #                               weight_space = valid_args['weight_space'])
+                    et_loss = self.mt_loss_fn.result()
+                
+                with trange(self.dataloader.info['test_step'], desc="Test steps") as t:
+                    self.mtt_loss_fn.reset_states()
+                    tt_metrics = []
+                    for test_step in t:
+                        t_data = test_iter.get_next()
+                        t_loss,t_metric = self._test_step(t_data['inputs'], t_data['labels'])
+                        t.set_postfix(test_loss=t_loss.numpy())
+                        tt_metrics.append(t_metric)
+                    ett_loss = self.mtt_loss_fn.result()
+                    ett_metric = tf.reduce_mean(tt_metrics)
+                    
+                e.set_postfix(et_loss=et_loss.numpy(), ett_metric=ett_metric.numpy(), ett_loss=ett_loss.numpy())
+                with self.logger.as_default():
+                    tf.summary.scalar("et_loss", et_loss, step=epoch)
+                    tf.summary.scalar("ev_loss", ev_loss, step=epoch)
+                    tf.summary.scalar("ett_mloss", ett_loss, step=epoch)
+                    tf.summary.scalar("ett_metric", ett_metric, step=epoch)
+        self.model.summary()
 
     def run(self, new_student=None, supervisor_info=None):
 
