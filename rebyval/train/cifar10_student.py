56d22faaaad766591a78e49be24e84de8267363a
zezzz <zezzz@zezzz.imac.lab>
2022-06-27 05:29:27 +0000

    test: cifar10-mlp-de

--- rebyval/train/cifar10_student.py
+++ rebyval/train/cifar10_student.py
@@ -42,11 +42,13 @@ class Cifar10Student(Student):
             with tf.GradientTape() as tape_s:
                 self.s_loss = self.weightspace_loss(self.model.trainable_variables)
             self.s_grad = tape_s.gradient(self.s_loss, self.model.trainable_variables)
-            
-        if epoch >= 30:
-            gradients = [(s/(1e-12 + tf.norm(s)))*decay_factor + t for s,t in zip(self.s_grad,t_grad)]
-        else:
-            gradients = t_grad
+        
+        gradients = [(s/(1e-12 + tf.norm(s)))*decay_factor + t for s,t in zip(self.s_grad,t_grad)]
+
+        # if epoch >= 30:
+        #     gradients = [(s/(1e-12 + tf.norm(s)))*decay_factor + t for s,t in zip(self.s_grad,t_grad)]
+        # else:
+        #     gradients = t_grad
             
         self.optimizer.apply_gradients(
             zip(gradients, self.model.trainable_variables))
