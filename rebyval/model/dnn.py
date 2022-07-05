58d76610ce15dbbac2ffbf8323530400e2f57de5
zhangenzhi <zhangsuiyu657@gmail.com>
2022-07-05 15:50:08 +0900

    test:soft act 10.

--- rebyval/model/dnn.py
+++ rebyval/model/dnn.py
@@ -133,9 +133,8 @@ class DNN(tf.keras.Model):
         return acts
 
     def call(self, inputs):
- 
+        
         if self.embedding:
-            ForkedPdb().set_trace()
             s_x = self.state_emb(inputs['state'])
             a_x = self.action_emb(inputs['action'])
             t_x = self.step_emb(inputs['step'])
