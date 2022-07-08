a526794af6daa1e97dc5c749935c59cc00e4e8e1
zezzz <zezzz@zezzz.imac.lab>
2022-07-08 09:14:29 +0000

    element-wish alpha.

--- rebyval/train/cifar10_rl_student.py
+++ rebyval/train/cifar10_rl_student.py
@@ -161,8 +161,8 @@ class Cifar10RLStudent(Student):
                         data = train_iter.get_next()
                         if self.supervisor == None:
                             train_loss, grads = self._train_step(data['inputs'], data['labels'])
-                            action = 1.0
                             act_grad = tf.concat([tf.reshape(tf.reduce_sum(g,axis=-1),(1,-1)) for g in grads], axis=-1)
+                            action = tf.ones(shape=act_grad.shape, dtype=tf.float32)
                         else:
                             train_loss, E_Q, action, act_grad, values = self._rl_train_step(data['inputs'], data['labels'])
                             with self.logger.as_default():
