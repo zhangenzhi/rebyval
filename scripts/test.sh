e4648aaecbcdf93ce00765a2af21fc1127a5fc3b
zhangenzhi <zhangsuiyu657@gmail.com>
2022-07-08 10:48:00 +0900

    ref: refact controller.

---
+++ scripts/test.sh
@@ -0,0 +1,3 @@
+python main.py --config="./scripts/configs/test/test_reval.yaml"
+python main.py --config="./scripts/configs/test/test_reval.yaml"
+python main.py --config="./scripts/configs/test/test_reval.yaml"
