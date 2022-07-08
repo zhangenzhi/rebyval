c8b89e2e1545c8ff1c89aef3c11e6b9d16b3e4d3
zezzz <zezzz@zezzz.imac.lab>
2022-07-08 08:01:01 +0000

    element-wish alpha.

--- rebyval/train/cifar10_rl_student.py
+++ rebyval/train/cifar10_rl_student.py
@@ -41,20 +41,20 @@ class Cifar10RLStudent(Student):
 
         import pdb
         pdb.set_trace()
-        scaled_gards = [g*a for g, a in zip(t_grad, self.action_sample)]
+        scaled_grads = [g*a for g, a in zip(t_grad, self.action_sample)]
         # for act in self.action_sample:
         #     scaled_gards.append([g*a for g, a in zip(t_grad, act)] )
 
-        flat_scaled_gards = []
-        for g in scaled_gards:
-            flat_scaled_gards.append([tf.reshape(tf.math.reduce_sum(g, axis= -1), shape=(-1)) for g in t_grad])
+        flat_scaled_gards = [tf.reshape(tf.math.reduce_sum(g, axis= -1), shape=(-1)) for g in scaled_grads]
+        # for g in scaled_grads:
+        #     flat_scaled_gards.append([tf.reshape(tf.math.reduce_sum(g, axis= -1), shape=(-1)) for g in t_grad])
         flat_scaled_gards = tf.concat(flat_scaled_gards, axis=0)
         
-        var_copy = tf.reshape(tf.tile(flat_var, [flat_scaled_gards.shape.as_list()[0], 1]), scaled_gards.shape)
+        var_copy = tf.reshape(tf.tile(flat_var, [flat_scaled_gards.shape.as_list()[0], 1]), scaled_grads.shape)
 
         # select wights with best Q-value
         steps = tf.reshape(tf.constant([self.gloabl_train_step/10000]*self.action_sample.shape[0], dtype=tf.float32),shape=(-1,1))
-        states_actions = {'state':var_copy, 'action':scaled_gards,'step':steps}
+        states_actions = {'state':var_copy, 'action':scaled_grads,'step':steps}
         self.values = self.supervisor(states_actions)
         return self.action_sample, self.values
 
