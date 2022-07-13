05aa5fb5c9b8d9569deae28e9c1ff71ea3140ca4
zezzz <zezzz@zezzz.imac.lab>
2022-07-13 09:49:48 +0000

    test: long epoch training.

--- rebyval/train/student.py
+++ rebyval/train/student.py
@@ -57,8 +57,8 @@ class Student(object):
         os.environ['CUDA_VISIBLE_DEVICES'] = devices
         gpus = tf.config.experimental.list_physical_devices("GPU")
         print_green("devices:", gpus)
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
+        # for gpu in gpus:
+        #     tf.config.experimental.set_memory_growth(gpu, True)
 
     def _build_dataset(self):
         # TODO: need dataloader registry
