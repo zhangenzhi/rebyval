a02475c61a07f0e87b3831af2efeaf39ba2819aa
zezzz <zezzz@zezzz.imac.lab>
2022-07-13 08:27:52 +0000

    test: long epoch training.

--- rebyval/train/cifar10_rl_student.py
+++ rebyval/train/cifar10_rl_student.py
@@ -2,7 +2,7 @@ from tqdm import trange
 import numpy as np
 import tensorflow as tf
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 # gpus = tf.config.experimental.list_physical_devices("GPU")
 # for gpu in gpus:
 #     tf.config.experimental.set_memory_growth(gpu, True)
