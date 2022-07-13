ba5a07d7b950ddce34080c69c52dbb8d89fb366f
zezzz <zezzz@zezzz.imac.lab>
2022-07-13 07:19:16 +0000

    test: long epoch training.

--- rebyval/controller/multi_controller.py
+++ rebyval/controller/multi_controller.py
@@ -16,7 +16,7 @@ class MultiController(BaseController):
         self.queue = Queue(maxsize=100)
 
     def _build_enviroment(self):
-        mp.set_start_method("fork")
+        # mp.set_start_method("fork")
 
         # self.gpus = tf.config.experimental.list_physical_devices("GPU")
         # for gpu in self.gpus:
