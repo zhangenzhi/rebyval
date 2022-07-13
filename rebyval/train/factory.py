e2b90764734a24e747fe927967397a0f8a3399ba
zezzz <zezzz@zezzz.imac.lab>
2022-07-13 08:22:01 +0000

    test: long epoch training.

--- rebyval/train/factory.py
+++ rebyval/train/factory.py
@@ -1,13 +1,10 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 from rebyval.train.cifar10_rl_student import Cifar10RLStudent
-from .cifar10_student import Cifar10Student
-from .cifar10_supervisor import Cifar10Supervisor
-
-import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
+from .cifar10_student import Cifar10Student
+from .cifar10_supervisor import Cifar10Supervisor
 
 from .mnist_student import MnistStudent
 from .mnist_supervisor import MnistSupervisor
@@ -18,8 +15,6 @@ from .cifar100_supervisor import Cifar100Supervisor
 from .cifar10_rl_student import Cifar10RLStudent
 from .cifar10_rl_supervisor import Cifar10RLSupervisor
 
-# import os
-# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 class StudentFactory():
     def __init__(self) -> None:
