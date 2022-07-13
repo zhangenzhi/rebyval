88200c44a8ab8902c492440e1a3345f4b3012287
zezzz <zezzz@zezzz.imac.lab>
2022-07-13 08:23:46 +0000

    test: long epoch training.

--- rebyval/train/factory.py
+++ rebyval/train/factory.py
@@ -1,7 +1,6 @@
 import os
 
-from rebyval.train.cifar10_rl_student import Cifar10RLStudent
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+from .cifar10_rl_student import Cifar10RLStudent
 
 from .cifar10_student import Cifar10Student
 from .cifar10_supervisor import Cifar10Supervisor
