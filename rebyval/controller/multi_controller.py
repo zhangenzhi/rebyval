1792426a2a30fb1648cb7e2b09fec22809021a34
zezzz <zezzz@zezzz.imac.lab>
2022-07-14 20:30:25 +0900

    test: gpus.

--- rebyval/controller/multi_controller.py
+++ rebyval/controller/multi_controller.py
@@ -93,8 +93,8 @@ class StudentProcess(mp.Process):
     def __init__(self, student, new_student=None, supervisor_info=None, devices='1'):
         super().__init__()
         print_green("Init Student:{} Process on Device:{}.".format(student.id, devices))
-        os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-        print_error(tf.config.experimental.list_physical_devices("GPU"))
+        # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+        # print_error(tf.config.experimental.list_physical_devices("GPU"))
         self.student = student 
         self.new_student = new_student
         self.supervisor_info = supervisor_info
