262091697ff7d3406392afc09cbcf9890863863f
zezzz <zezzz@zezzz.imac.lab>
2022-07-14 21:24:02 +0900

    test: gpus.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from rebyval.tools.utils import get_yml_content
 from rebyval.controller.utils import check_args_from_input_config
 
 from rebyval.controller.base_controller import BaseController
-from rebyval.controller.dist_controller import DistController
+# from rebyval.controller.dist_controller import DistController
 from rebyval.controller.multi_controller import MultiController
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
