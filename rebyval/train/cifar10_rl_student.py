f0767ba6702e1de54362d7e93b4bba976c7c4af6
zezzz <zezzz@zezzz.imac.lab>
2022-07-21 08:21:59 +0000

    test: long epoch training.

--- rebyval/train/cifar10_rl_student.py
+++ rebyval/train/cifar10_rl_student.py
@@ -2,16 +2,6 @@ from tqdm import trange
 import numpy as np
 import tensorflow as tf
 
-# import os
-# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-
-# from rebyval.train.utils import ForkedPdb
-# ForkedPdb().set_trace()
-
-# gpus = tf.config.experimental.list_physical_devices("GPU")
-# for gpu in gpus:
-#     tf.config.experimental.set_memory_growth(gpu, True)
-
 # others
 import time
 from rebyval.train.student import Student
