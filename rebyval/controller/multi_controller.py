5b805687b49fc5aaa8c605e973757d8f8638d741
zezzz <zezzz@zezzz.imac.lab>
2022-07-28 21:10:55 +0900

    feat: bootstrap Q value update.

--- rebyval/controller/multi_controller.py
+++ rebyval/controller/multi_controller.py
@@ -99,7 +99,7 @@ class StudentProcess(mp.Process):
     def run(self):
 
         # gpus = tf.config.experimental.list_physical_devices("GPU")
-        print_red("Init Student:{} Process on Device:{}.".format(self.student.id, gpus))
+        # print_red("Init Student:{} Process on Device:{}.".format(self.student.id, gpus))
         self.student.run(new_student=self.new_student, supervisor_info=self.supervisor_info, devices=self.devices)
 
 
