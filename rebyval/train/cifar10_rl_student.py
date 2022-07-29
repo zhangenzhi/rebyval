f4038c12e51e4f9ace3dd485dd4773440399abc0
zezzz <zezzz@zezzz.imac.lab>
2022-07-29 19:30:26 +0900

    feat: bootstrap Q value update.

--- rebyval/train/cifar10_rl_student.py
+++ rebyval/train/cifar10_rl_student.py
@@ -87,6 +87,8 @@ class Cifar10RLStudent(Student):
     def fix_action(self, t_grad):
         flat_weights = [tf.reshape(tf.math.reduce_sum(v, axis= -1), shape=(-1)) for v in self.model.trainable_variables] 
         state = tf.reshape(tf.concat(flat_weights, axis=0), (1,-1))
+        import pdb
+        pdb.set_trace()
         self.values = self.supervisor(state)
         return self.action_sample, self.values
     
