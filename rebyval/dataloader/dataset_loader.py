0b6d1427608e59c97256f98e850b4028d1743023
zezzz <zezzz@zezzz.imac.lab>
2022-08-11 18:29:58 +0900

    feat: TD with N action-values.

--- rebyval/dataloader/dataset_loader.py
+++ rebyval/dataloader/dataset_loader.py
@@ -129,7 +129,7 @@ class Cifar10DataLoader(BaseDataLoader):
 
         test_dataset = tf.data.Dataset.from_tensor_slices({'inputs': x_test, 'labels': y_test})
         # all 1w test
-        test_dataset = test_dataset.shuffle(test_size).batch(self.dataloader_args['batch_size']).repeat(epochs)
+        test_dataset = test_dataset.batch(self.dataloader_args['batch_size']).repeat(epochs)
         valid_dataset = test_dataset
         
         # test_dataset = test_dataset.shuffle(test_size)
